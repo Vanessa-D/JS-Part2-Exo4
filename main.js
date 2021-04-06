c7327010eb07ee8be9f2318e2891567d91a7c482
Vanessa-D <vanessa.ducloux@laposte.net>
2021-04-06 13:31:53 +0200

    first

---
+++ main.js
@@ -0,0 +1,6 @@
+// Fonction qui au clique indique une non-valeur pour chaque id du DOM
+function wipeOff(){
+    document.getElementById("surname").value='';
+    document.getElementById("firstname").value='';
+    document.getElementById("city").value='';
+}
